4cf924fe5cbf10424f6b1e952a9ad75cb0db1461
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2024-01-01 14:36:40 +0000

    Bug Fixed, add Meta and str to models

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-vitaliichepurny-blogapp-30f3zb8unsq.ws-eu107.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-vitaliichepurny-blogapp-wy1sfe0s0kj.ws-eu107.gitpod.io', '.herokuapp.com']
 
 # Application definition
 
