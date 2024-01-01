60613cd03ee656d3c16ad13238ec5c6fe914e977
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2024-01-01 17:02:43 +0000

    Manually populated posts advansing admin panel

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-vitaliichepurny-blogapp-wy1sfe0s0kj.ws-eu107.gitpod.io', '.herokuapp.com']
 
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'django_summernote',
+    'blog',
 ]
 
 MIDDLEWARE = [
