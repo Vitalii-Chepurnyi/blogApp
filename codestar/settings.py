d18448d85fe858d187bb052ca108926f78a578b1
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2024-01-05 20:01:57 +0000

    Add about app connected to data

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-vitaliichepurny-blogapp-63z48m1b49p.ws-eu107.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-vitaliichepurny-blogapp-znjhg2h2p79.ws-eu107.gitpod.io', '.herokuapp.com']
 
 # Application definition
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
